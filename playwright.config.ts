e1cfdfb2a52915cca012277a82e3fdb25694ce78
sxthacker <sxthacker@gmail.com>
2024-03-12 00:36:43 -0700

    initial playwright project

---
+++ playwright.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+  use: {
+    // baseURL: "http://127.0.0.1:3000",
+    screenshot: "only-on-failure",
+    trace: "on-first-retry",
+  },
+
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+    {
+       name: "chrome",
+       use: { ...devices["Desktop Chrome"], channel: "chrome" },
+    },
+  ],
+});
